85781caa1988cf23d1dd6941249db91444cdc4dc
Student <example@example.com>
2020-01-06 15:51:26 -0800

    Add MadLibs20Test files.

---
+++ MadLibs20Test/Model.Test/ScaryMadLibVariable.cs
@@ -0,0 +1,10 @@
+namespace MadLibs20.Models
+{
+    public class ScaryMadLibVariable
+    {
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+        public string Location { get; set; }
+        public string Souvenir { get; set; }
+    }
+}
